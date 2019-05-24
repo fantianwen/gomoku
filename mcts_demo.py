35a2426b93c785fd536f356433f04d39134c8577
fantianwen <fantianwen121@gmail.com>
2019-05-24 21:40:52 +0900

    git cache remove

--- mcts_demo.py
+++ mcts_demo.py
@@ -9,8 +9,8 @@ board = Board(width=9, height=9)
 player1 = PureMCTSPlayer(Board.kPlayerBlack, name="AI 1",weight_c=5, compute_budget=20000)
 # player1 = HumanPlayer(Board.kPlayerBlack, name="player 1")
 
-# player2 = PureMCTSPlayer(Board.kPlayerWhite, name="AI 2", weight_c=5, compute_budget=5000)
-player2 = HumanPlayer(Board.kPlayerWhite, name="player 2")
+player2 = PureMCTSPlayer(Board.kPlayerWhite, name="AI 2", weight_c=5, compute_budget=5000)
+# player2 = HumanPlayer(Board.kPlayerWhite, name="player 2")
 
 server = GameServer(board, GameServer.kNormalPlayGame, player1, player2)
 server.startGame()
